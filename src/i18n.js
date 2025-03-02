6c13c27f0ec9fed4d3fe4d14b28387b9fdde967e
Egorpuzik <egor.egorov0004@gmail.com>
2025-03-02 13:58:50 +0100

    третья вкладка

---
+++ src/i18n.js
@@ -0,0 +1,25 @@
+import i18next from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Импорт локальных JSON-файлов с переводами
+import translationEN from './locales/en.json';
+import translationRU from './locales/ru.json';
+
+i18next
+  .use(LanguageDetector) // Определение языка пользователя
+  .use(initReactI18next) // Интеграция с React
+  .init({
+    resources: {
+      en: { translation: translationEN },
+      ru: { translation: translationRU },
+    },
+    fallbackLng: 'en', // Язык по умолчанию
+    debug: true, // Включаем логи в консоли
+    interpolation: {
+      escapeValue: false, // Отключаем экранирование HTML
+    },
+  });
+
+export default i18next;
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import app from './app.js';
+import './i18n';
 
 document.addEventListener('DOMContentLoaded', app);
