25db2f689a833c4724b0b905291e2b6a44cf7d01
Egorpuzik <egor.egorov0004@gmail.com>
2025-03-31 15:22:15 +0200

    fix #9

--- src/api/fetchRSS.js
+++ src/api/fetchRSS.js
@@ -6,13 +6,19 @@ const fetchRSS = async (url) => {
   try {
     const apiUrl = new URL(PROXY_URL);
     apiUrl.searchParams.append('disableCache', 'true');
-    apiUrl.searchParams.append('url', url);
+    apiUrl.searchParams.append('url', encodeURIComponent(url)); 
+
+    console.log("Запрашиваемый URL:", apiUrl.toString()); 
 
     const response = await axios.get(apiUrl.toString());
+
+    console.log("Ответ от сервера:", response); 
 
     if (!response.data || !response.data.contents) {
       throw new Error('Ошибка загрузки RSS');
     }
+
+    console.log("Полученный XML:", response.data.contents); 
 
     return response.data.contents;
   } catch (error) {
